bb69ba006b6cd608585fa980804aac7188a21e1a
Daniel Hedin <d.hedin@gmail.com>
2023-01-24 13:40:53 +0100

    Added lecture 2

---
+++ Lecture2/Lecture2/Clock.cs
@@ -0,0 +1,52 @@
+﻿
+using System.Timers;
+
+namespace Lecture2
+{
+    class Clock
+    {
+
+        int Hour, Minute, Second;
+
+        public Clock(int hour, int minute, int second)
+        {
+            Set(hour, minute, second);
+        }
+
+        private void TimerTick(object? sender, ElapsedEventArgs e)
+        {
+            Tick();
+        }
+
+        public void Tick()
+        {
+            Second++;
+            if (Second >= 60)
+            {
+                Second = 0;
+                Minute++;
+            }
+            if (Minute >= 60)
+            {
+                Minute = 0;
+                Hour++;
+            }
+            if (Hour >= 24)
+            {
+                Hour = 0;
+            }
+        }
+
+        public void Set(int hour, int minute, int second)
+        {
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+        }
+
+        public string GetTime()
+        {
+            return $"{Hour}:{Minute}:{Second}";
+        }
+    }
+}
---
+++ Lecture2/Lecture2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lecture2;
+
+var clock = new Clock(12, 0, 0);
+clock.Tick();
+
+Console.WriteLine(clock.GetTime());
